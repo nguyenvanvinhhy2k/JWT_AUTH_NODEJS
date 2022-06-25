666d4e9625c9641eb0d5d90ee2a6439b75caa245
Vinh Nguyen <nguyenvanvinhhy2k@gmail.com>
2022-06-25 17:43:00 +0700

    ok

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 // const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const port = 8000;
 
 dotenv.config();
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("Connect mongodb success");
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan("combined"));
